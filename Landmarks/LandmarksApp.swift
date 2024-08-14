396a791c516d05862e42683373aa7143e8db3539
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-14 23:34:55 +0900

    Firebase SDK を追加

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -6,9 +6,22 @@ The top-level definition of the Landmarks app.
 */
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @main
 struct LandmarksApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     @State private var modelData = ModelData()
 
     var body: some Scene {
