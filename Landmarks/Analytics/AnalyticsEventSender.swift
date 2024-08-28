499bd281cac7820cfdf1ca63e887f33bb3c1c197
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-08-29 02:03:56 +0900

    Kotlin Multiplatform で定義したイベントクラスを作成する

--- Landmarks/Analytics/AnalyticsEventSender.swift
+++ Landmarks/Analytics/AnalyticsEventSender.swift
@@ -1,4 +1,5 @@
 import FirebaseAnalytics
+import ga913kmp
 
 struct AnalyticsEventSender {
     func sendScreen(_ screen: AnalyticsEventScreen) {
@@ -12,6 +13,26 @@ struct AnalyticsEventSender {
     }
 
     func sendAction(_ action: AnalyticsEventAction) {
+        if action.isConversionEvent {
+            Analytics.logEvent(action.eventName, parameters: action.eventParameters)
+        } else {
+            var parameters = action.eventParameters
+            parameters["ga913_action_name"] = action.eventName
+            Analytics.logEvent(AnalyticsEventSelectContent, parameters: parameters)
+        }
+    }
+
+    func sendScreen(_ screen: KmpAnalyticsEventScreen) {
+        if screen.isConversionEvent {
+            Analytics.logEvent(screen.eventName, parameters: nil)
+        } else {
+            Analytics.logEvent(AnalyticsEventScreenView, parameters: [
+                AnalyticsParameterScreenName: screen.eventName,
+            ])
+        }
+    }
+
+    func sendAction(_ action: KmpAnalyticsEventAction) {
         if action.isConversionEvent {
             Analytics.logEvent(action.eventName, parameters: action.eventParameters)
         } else {
